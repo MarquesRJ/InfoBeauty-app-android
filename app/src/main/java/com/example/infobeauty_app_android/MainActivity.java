83f390b80098c233ccbd68d305b10a504c5bc43a
MarquesRJ <gabrielmarques142@gmail.com>
2021-04-21 19:46:55 -0300

    Main e DB

--- app/src/main/java/com/example/infobeauty_app_android/MainActivity.java
+++ app/src/main/java/com/example/infobeauty_app_android/MainActivity.java
@@ -1,12 +1,22 @@
 package com.example.infobeauty_app_android;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.solver.state.State;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import com.example.infobeauty_app_android.database.DadosOpenHelper;
+import com.google.android.material.snackbar.Snackbar;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout
+    private SQLiteDatabase conexao;
+    private DadosOpenHelper dadosOpenHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +24,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    private void criarconexao(){
+
+        try {
+
+            dadosOpenHelper = new DadosOpenHelper(this);
+
+            conexao = dadosOpenHelper.getWritableDatabase();
+
+            Snackbar.make():
+
+        }catch (SQLException ex){
+
+
+        }
+
+
+
+    }
     public void telaIndexUsuario(View view){
 
         Intent intent = new Intent(this, IndexUsuario.class);
